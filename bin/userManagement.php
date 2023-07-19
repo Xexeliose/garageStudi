5b3afb133f9e7e61de29fd8b411ca9a81021dfeb
François BRETILLON <fbretillon@outlook.com>
2023-07-19 12:39:37 +0200

    CSS fix

--- bin/userManagement.php
+++ bin/userManagement.php
@@ -12,8 +12,8 @@ $pageTitle = "Gestion Utilisateur";
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <!-- My CSS-->
-    <link rel="stylesheet" href="/css/template.css">
-    <link rel="stylesheet" href="/css/userManagement.css">
+    <link rel="stylesheet" href="css/template.css">
+    <link rel="stylesheet" href="css/userManagement.css">
 
 </head>
 
